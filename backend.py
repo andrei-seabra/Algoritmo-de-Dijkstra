dfc7b37e181780f7a6b7fccbf2a341e1a3735cba
ANDREI AMASILIO SEABRA <15826294639@pedag.local>
2025-11-10 15:21:20 -0300

    feat: dijkstra algorithm

---
+++ backend.py
@@ -0,0 +1,26 @@
+import math, heapq
+
+def dijkstra(adjacencyList, source):
+    distance = {vertex: math.inf for vertex in adjacencyList}
+    previous = {vertex: None for vertex in adjacencyList}
+    distance[source] = 0
+    priorityQueue = [(0, source)]
+    visited = set()
+
+    while priorityQueue:
+        currentDistance, currentVertex = heapq.heappop(priorityQueue)
+
+        if currentVertex in visited:
+            continue
+
+        visited.add(currentVertex)
+
+        for neighbourVertex, weight in adjacencyList.get(currentVertex, []):
+            alternativeDistance = currentDistance + weight
+
+            if alternativeDistance < distance[neighbourVertex]:
+                distance[neighbourVertex] = alternativeDistance
+                previous[neighbourVertex] = currentVertex
+                heapq.heappush(priorityQueue, (alternativeDistance, neighbourVertex))
+
+    return distance, previous
